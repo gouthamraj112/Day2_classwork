c2e21d1a69897a71418c51a105b96541a9a38bae
gouthamraj112 <goutham.raj@prodapt.com>
2026-07-17 17:49:25 +1300

    Update example3.js

--- day-10/example3.js
+++ day-10/example3.js
@@ -2,6 +2,7 @@ let marks = [85, 42, 78, 90, 38, 76, 55, 29, 84, 49];
 let passed = 0;
 let failed = 0;
 let max = 0;
+let min=marks[0];
 
 
 for (let i = 0; i < marks.length; i++) {
@@ -15,8 +16,13 @@ for (let i = 0; i < marks.length; i++) {
     if(max<marks[i]){
         max=marks[i];
     }
+    if(min>marks[i])
+    {
+        min=marks[i];
+    }
 }
 
 console.log("Total Passed: " + passed);
 console.log("Total Failed: " + failed);
 console.log("Highest Marks: " + max);
+console.log("Least Marks:"+min )
